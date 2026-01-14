0f43f5717ecfe05c92a566b9338427cd843382f5
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-01-14 22:00:15 +0500

    PDF and Invoice Num COntroller Removed to make code more secure

--- backend/src/routes/sales.routes.js
+++ backend/src/routes/sales.routes.js
@@ -5,13 +5,10 @@ import {
   removeSale,
   viewSale,
 } from "../controllers/salesHistory.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router
-  .route("/add-sale")
-  .post(authentication, upload.single("invoice"), addSale);
+router.route("/add-sale").post(authentication, addSale);
 router.route("/remove-sale/:saleId").delete(authentication, removeSale);
 router.route("/view-sale/:saleId").get(authentication, viewSale);
 
