62053c738ad8bd20cf6152323216dd4339a5ab3b
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-01-14 14:34:28 +0500

    Container Added

--- frontend/src/components/container/Container.jsx
+++ frontend/src/components/container/Container.jsx
@@ -1,3 +1,9 @@
 export function Container({ children, className }) {
-  return <div className={`${className}`}>{children}</div>;
+  return (
+    <div
+      className={`mx-auto w-full max-w-4xl rounded-xl border border-slate-200 bg-white p-6 shadow-lg shadow-slate-200/50 md:p-8 ${className}`}
+    >
+      {children}
+    </div>
+  );
 }
