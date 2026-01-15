3ba8bc3892e88cf54d263c0ccc454693ca1f7666
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-01-15 16:22:10 +0500

    App.jsx Implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { axiosInstance } from "./axios/axios";
+import { Container, Loading, NavBar } from "./components";
+import { Outlet } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true);
+  const [alert, setAlert] = useState("");
+  const dispatch = useDispatch();
 
-  return (
-    <>
-    </>
-  )
+  useEffect(async () => {
+    try {
+      const response = await axiosInstance.get("/api/v1/user/current-user");
+      if (response?.status === 200) dispatch({ userData: response?.data });
+    } catch (error) {
+      setAlert(error?.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [dispatch]);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <div className="min-h-screen w-full bg-slate-50 font-sans text-slate-900 antialiased selection:bg-indigo-100 selection:text-indigo-700">
+     
+      <Container className="max-w-full! bg-transparent! p-0! shadow-none! border-none! relative flex flex-col">
+        <NavBar />
+
+        <main className="mx-auto w-full max-w-7xl px-4 pb-20 pt-28 sm:px-6 lg:px-8">
+          <Outlet />
+        </main>
+      </Container>
+    </div>
+  );
 }
 
-export default App
+export default App;
