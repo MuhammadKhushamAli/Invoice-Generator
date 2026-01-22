594fd98c39fe6d950a66a864961dcee170cef355
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-01-22 18:19:28 +0500

    Unnecessary import removed

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { ApiError } from "./utils/ApiError.js";
-import puppeteer from "puppeteer";
 
 const app = express();
 
