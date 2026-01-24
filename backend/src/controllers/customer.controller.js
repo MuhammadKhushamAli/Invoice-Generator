7e2b10b11d3b6786f545282e9d81fe7f69a424f7
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-01-24 18:45:07 +0500

    Cusotmer res setuped

--- backend/src/controllers/customer.controller.js
+++ backend/src/controllers/customer.controller.js
@@ -6,10 +6,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 export const getCustomers = asyncHandler(async (req, res) => {
   const customers = await Customer.find({
     owner: req?.user?._id,
-  });
+  }). select("-owner -__v -quotations -deliveryChalan -invoices");
   if (!customers) throw new ApiError(500, "Unable to Fetch Customers");
 
-  res
+  return res
     .status(200)
     .json(new ApiResponse(200, "Customers Fetched Successfully", customers));
 });
